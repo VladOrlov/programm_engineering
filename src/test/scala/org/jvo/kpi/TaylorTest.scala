6fe2eccec8c11ae534def14692ea94b9eb972df1
Vlad Orlov <Ghost_@ukr.net>
2021-04-14 23:14:57 +0300

    Merge with master

--- src/test/scala/org/jvo/kpi/TaylorTest.scala
+++ src/test/scala/org/jvo/kpi/TaylorTest.scala
@@ -7,14 +7,14 @@ class TaylorTest extends AnyFunSpec {
   describe("Taylor functions for 0.3") {
     describe("with taylor series 4") {
       it("should be equal to 2.166240677847414") {
-        assert(Taylor.calculate(0.3, 4) === 2.166240677847414)
+        assert(Taylor.calculateA(0.3, 4) === 2.166240677847414)
       }
     }
 
     describe("with taylor series 0") {
       it("should produce IllegalArgumentException") {
         assertThrows[IllegalArgumentException] {
-          Taylor.calculate(0.3, 0)
+          Taylor.calculateA(0.3, 0)
         }
       }
     }

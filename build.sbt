d4ebdccf810783a3aa98a710c1caee6e4d1336ec
Vlad Orlov <Ghost_@ukr.net>
2021-04-14 23:10:09 +0300

    Add unit tests

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "programm_engineering"
 version := "1.0.1"
 
 scalaVersion := "2.12.13"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.7" % Test
--- src/main/scala/org/jvo/kpi/Taylor.scala
+++ src/main/scala/org/jvo/kpi/Taylor.scala
@@ -10,11 +10,12 @@ object Taylor {
   }
 
   def calculate(x: Double, taylorSeries: Int): Double = {
+    require(taylorSeries > 0)
     (1 to taylorSeries).toList
       .foldLeft(0.0)((z: Double, series: Int) => {
         println(s"n = $series")
         val taylor = z + 1 / math.pow(1 + x, series)
-        println(f"taylor = $taylor%1.5f")
+        println(s"taylor = $taylor")
         taylor
       })
   }
